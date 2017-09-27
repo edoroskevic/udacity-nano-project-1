1aa034156dc03089cc30504d0166fc561cc40c50
e.doroskevic <e_doroskevic@hotmail.com>
2017-09-27 13:47:22 +0100

    Initial Commit

---
+++ entertainment_center.py
@@ -0,0 +1,45 @@
+"""
+Project: Movie Trailer Website
+
+Description: A project developed as part of Udacity nanodegree in
+             Full-stack Web Development. This projects demonstrates
+             knowledge of fundamental Object Oriented Programming (OOP)
+             principals, as well as showcases the ability to utilize
+             this knowledge using Python programming language.
+
+Developer: Eduard Doroskevic
+Contact: e_doroskevic@hotmail.com
+Date: 27th of September 2017
+
+File(s):
+    entertainment_center.py , media.py, fresh_tomatoes.py
+"""
+
+# Loading necessary module components.
+from media import Movie
+from fresh_tomatoes import open_movies_page
+
+# Instantiating three objects of type Movie.
+movieYourHighness = Movie(
+    "Your Highness",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/3/31/Your_Highness_Poster.jpg/220px-Your_Highness_Poster.jpg",
+    "https://youtu.be/FplWxtPzWY8"
+    )
+
+movieHackers = Movie(
+    "Hackers",
+    "https://uk.movieposter.com/posters/archive/main/79/MPW-39928",
+    "https://youtu.be/Rn2cf_wJ4f4"
+    )
+
+movieHacksawRidge = Movie(
+    "Hacksaw Ridge",
+    "https://upload.wikimedia.org/wikipedia/en/8/8a/Hacksaw_Ridge_poster.png",
+    "https://youtu.be/s2-1hz1juBI"
+    )
+
+# Creating a list to hold created movies above.
+movies = [movieYourHighness, movieHackers, movieHacksawRidge]
+
+# Generating .html file
+open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,39 @@
+"""
+Project: Movie Trailer Website
+
+Description: A project developed as part of Udacity nanodegree in
+             Full-stack Web Development. This projects demonstrates
+             knowledge of fundamental Object Oriented Programming (OOP)
+             principals, as well as showcases the ability to utilize
+             this knowledge using Python programming language.
+
+Developer: Eduard Doroskevic
+Contact: e_doroskevic@hotmail.com
+Date: 27th of September 2017
+
+File(s):
+    entertainment_center.py , media.py, fresh_tomatoes.py
+"""
+
+class Movie():
+    """
+    A class describing properties and methods of a Movie instance.
+    """
+
+    def __init__(self, title, art, trailer):
+        """
+            Initialize an instance of a Movie class
+
+            Arg(s):
+                title - a string representing the title of the movie
+                art - a string representing the URL to the poster of the movie
+                trailer - a string representing URL to the trailer of the movie
+
+            Example(s):
+                title - 'The Godfather'
+                art - 'https://vignette3.wikia.nocookie.net/mafia/images/b/b8/The-godfather.jpg/revision/latest?cb=20080922232324'
+                trailer - 'https://youtu.be/sY1S34973zA'
+        """
+        self.title = title;
+        self.poster_image_url = art;
+        self.trailer_youtube_url = trailer;
